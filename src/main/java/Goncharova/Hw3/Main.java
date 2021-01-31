6a1bb79fa40491d4f41398777a751ffac5e015d8
Sergey Goncharov <sergeishk@gmail.com>
2021-01-31 21:27:32 +0300

    Добавлена генерация 3 случайных чисел

--- src/main/java/Goncharova/Hw3/Main.java
+++ src/main/java/Goncharova/Hw3/Main.java
@@ -1,11 +1,27 @@
 package Goncharova.Hw3;
 
-public class Main {
-   public static void main (String[] args) {
+import java.util.Random;
 
+public class Main {
+
+   static Random random = new Random();
+
+
+   public static void main (String[] args) {
+      Game1();
    }
 
    public static boolean Game1() {
+      int number = 0;
+      for(int i = 0; i < 3; i++){
+         number = random.nextInt(9);
+         System.out.println(number);
+      }
+
+
+
+
+
       return false;
    }
 }
