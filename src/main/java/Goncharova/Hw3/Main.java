95f0db7ec0147e6b2568860841cc81b1f6c28d07
Sergey Goncharov <sergeishk@gmail.com>
2021-01-31 22:02:01 +0300

    Добавлен запрос на новую игру

--- src/main/java/Goncharova/Hw3/Main.java
+++ src/main/java/Goncharova/Hw3/Main.java
@@ -9,37 +9,43 @@ public class Main {
         Game1();
     }
 
-    public static boolean Game1() {
+    public static void Game1() {
         int number = 0;
         int userAnswer = 0;
+        boolean inGame = true;
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
 
 
-        number = random.nextInt(9);
-        System.out.println(number);
+        while (inGame == true) {
+            number = random.nextInt(9);
+            System.out.println(number);
 
-        for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < 3; i++) {
 
-            System.out.print("Введите число от 0 до 9: ->>");
+                System.out.print("Введите число от 0 до 9: ->>");
+                userAnswer = scanner.nextInt();
+
+
+                if (userAnswer == number) {
+                    System.out.println("Поздравляем, Вы угадали!");
+                    break;
+                } else {
+                    if (userAnswer > number) {
+                        System.out.println("Указанное Вами число оказалось больше загаданного");
+                    }
+                    if (userAnswer < number) {
+                        System.out.println("Указанное Вами число оказалось меньше загаданного");
+                    }
+                }
+            }
+
+            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
             userAnswer = scanner.nextInt();
+            inGame = (userAnswer == 1);
 
-            if (userAnswer == number) {
-                System.out.println("Поздравляем, Вы угадали!");
-                break;
-            }
-            else {
-                if (userAnswer > number) {
-                    System.out.println("Указанное Вами число оказалось больше загаданного");
-                }
-                if (userAnswer < number) {
-                    System.out.println("Указанное Вами число оказалось меньше загаданного");
-                }
-            }
         }
 
-
         scanner.close();
-        return false;
     }
 }
