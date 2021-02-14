c3677a9292ed9c5a0893f0061f081dbe0b7add7f
Alina Goncharova <al.cherepnina@gmail.com>
2021-02-14 22:27:27 +0300

    Добавлен класс Plate

--- src/main/java/Goncharova/Hw3/Cat.java
+++ src/main/java/Goncharova/Hw3/Cat.java
@@ -1,7 +1,16 @@
 package Goncharova.Hw3;
 
 public class Cat extends Animal {
-    Cat() {
+    private static int Counter;
+    private boolean saturation;
+
+    Cat()  {
+        super("Cat", false);
+        this.runDistanseLimit = 200;
+        this.swimDistanseLimit = 5;
+        saturation = false;
+        Counter++;
+        System.out.println("Количество созданных котов " + Counter);
 
     }
 
@@ -9,10 +18,20 @@ public class Cat extends Animal {
         super(name, canSwim);
         this.runDistanseLimit = 200;
         this.swimDistanseLimit = 5;
+        saturation = false;
         Counter++;
         System.out.println("Количество созданных котов " + Counter);
     }
+    public void eat(Plate plate, int n) {
+        saturation = plate.decreaseFood(n);
+    }
+    public void printSaturation() {
+        if (saturation == true) {
+            System.out.println("Кот сыт!");
+        } else {
+            System.out.println("Кот голоден!");
+        }
 
-    private static int Counter;
+    }
 
 }
--- src/main/java/Goncharova/Hw3/Main.java
+++ src/main/java/Goncharova/Hw3/Main.java
@@ -3,34 +3,23 @@ package Goncharova.Hw3;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Создаем котов:");
+        Cat [] cats = new Cat [5];
 
-        Cat cat = new Cat("Мурзик", false);
-        Cat cat1 = new Cat("Маркиз", true);
-        Dog dog = new Dog("Коржик");
-        Dog dog1 = new Dog("Джек");
+        for (int i = 0; i < cats.length; i++) {
+            cats[i] = new Cat();
+        }
 
-        cat.run(160);
-        cat.swim(4);
-        cat1.run(201);
-        cat1.swim(5);
+        System.out.println("Начинаем кормить котов:");
 
-        dog.run(10);
-        dog.swim(9);
-        dog1.run(501);
-        dog1.swim(11);
+            Plate plate = new Plate (100);
 
-//        Exployer[] persArray = new Exployer[5]; // Вначале объявляем массив объектов
-//        persArray[0] = new Exployer("Дмитрий", "Программист", "dmitriy@gmail.com", "89991234567", 30000, 37); // потом для каждой ячейки массива задаем объект
-//        persArray[1] = new Exployer("Алексей", "Лаборант", "alex@gmail.com", "8999345543", 2500, 30);
-//        persArray[2] = new Exployer("Владислав", "Тестировщик", "vlad@gmail.com", "899934565", 3000, 42);
-//        persArray[3] = new Exployer("Марк", "Инженер", "mark@gmail.com", "899934111", 2700, 28);
-//        persArray[4] = new Exployer("Владимир", "Директор", "vladimir@gmail.com", "89995354", 4000, 42);
-//
-//        for (int i = 0; i < persArray.length; i++) {
-//            if (persArray[i].getAge() > 40) {
-//                persArray[i].info();
-//            }
-//        }
+        for (int i = 0; i < cats.length; i++) {
+            cats[i].eat(plate, 50);
+            System.out.println("Кормим кота " + (i+1));
+            cats[i].printSaturation();
+            plate.info();
+        }
 
     }
 }
