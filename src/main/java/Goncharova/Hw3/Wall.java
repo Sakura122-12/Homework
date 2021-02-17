9e2fc8a78c5f7986ef29e775ad63da917a26f776
Alina Goncharova <al.cherepnina@gmail.com>
2021-02-17 22:33:50 +0300

    Добавлен класс Wall с конструктором по умолчанию, с конструктором с параметрами и полем Высота

--- src/main/java/Goncharova/Hw3/Wall.java
+++ src/main/java/Goncharova/Hw3/Wall.java
@@ -1,7 +1,20 @@
 package Goncharova.Hw3;
 
 public class Wall {
+
+    private float height;
+    public float getHeight(){
+        return height;
+    }
+
     Wall() {
+        height = 1.0f;
 
     }
+
+    Wall(float height) {
+        this.height = height;
+    }
+
+
 }
