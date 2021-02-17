5b18a58893115df27ddd63bc715a96b16a5775f5
Alina Goncharova <al.cherepnina@gmail.com>
2021-02-17 22:13:49 +0300

    Добавлен класс Cat

--- src/main/java/Goncharova/Hw3/Cat.java
+++ src/main/java/Goncharova/Hw3/Cat.java
@@ -1,37 +1,15 @@
 package Goncharova.Hw3;
 
-public class Cat extends Animal {
-    private static int Counter;
-    private boolean saturation;
+public class Cat {
 
-    Cat()  {
-        super("Cat", false);
-        this.runDistanseLimit = 200;
-        this.swimDistanseLimit = 5;
-        saturation = false;
-        Counter++;
-        System.out.println("Количество созданных котов " + Counter);
+    Cat(){
 
     }
-
-    Cat(String name, boolean canSwim) {
-        super(name, canSwim);
-        this.runDistanseLimit = 200;
-        this.swimDistanseLimit = 5;
-        saturation = false;
-        Counter++;
-        System.out.println("Количество созданных котов " + Counter);
-    }
-    public void eat(Plate plate, int n) {
-        saturation = plate.decreaseFood(n);
-    }
-    public void printSaturation() {
-        if (saturation == true) {
-            System.out.println("Кот сыт!");
-        } else {
-            System.out.println("Кот голоден!");
-        }
-
+    public void run(float distanse) {
+        System.out.println("Кот пробежал " + distanse + " метров");
     }
 
+    public void jump(float height) {
+        System.out.println("Кот прыгнул " + height + " метров");
+    }
 }
