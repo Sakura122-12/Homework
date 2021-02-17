2409a375f88758c86b35d1f9fa0db0500c8294e9
Alina Goncharova <al.cherepnina@gmail.com>
2021-02-17 22:12:28 +0300

    Добавлен класс Human

---
+++ src/main/java/Goncharova/Hw3/Human.java
@@ -0,0 +1,15 @@
+package Goncharova.Hw3;
+
+public class Human {
+
+    Human(){
+
+    }
+    public void run(float distanse) {
+        System.out.println("Человек пробежал " + distanse + " метров");
+    }
+
+    public void jump(float height) {
+        System.out.println("Человек прыгнул " + height + " метров");
+    }
+}
