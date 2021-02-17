5b631f837eaf336cfb5fd8d14d4da2ae4ce0506b
Alina Goncharova <al.cherepnina@gmail.com>
2021-02-17 22:16:14 +0300

    Добавлен класс Robot

---
+++ src/main/java/Goncharova/Hw3/Robot.java
@@ -0,0 +1,15 @@
+package Goncharova.Hw3;
+
+public class Robot {
+
+    Robot(){
+
+    }
+    public void run(float distanse) {
+        System.out.println("Робот пробежал " + distanse + " метров");
+    }
+
+    public void jump(float height) {
+        System.out.println("Робот прыгнул " + height + " метров");
+    }
+}
