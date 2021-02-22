775d3a3001b3c428b411a24d410f29b628945185
Alina Goncharova <al.cherepnina@gmail.com>
2021-02-22 12:42:55 +0300

    Добавлены массивы для проверки и запуск функции с ошибочного массива

--- src/main/java/Goncharova/Hw3/Main.java
+++ src/main/java/Goncharova/Hw3/Main.java
@@ -3,29 +3,68 @@ package Goncharova.Hw3;
 public class Main {
 
     public static void main(String[] args) {
-        String [][] array = new String[4][4];
-        array[0][0] = "5"; array[0][1] = "4"; array[0][2] = "3"; array[0][3] = "2";
-        array[1][0] = "f"; array[1][1] = "8"; array[1][2] = "9"; array[1][3] = "10";
-        array[2][0] = "11"; array[2][1] = "12"; array[2][2] = "13"; array[2][3] = "14";
-        array[3][0] = "15"; array[3][1] = "16"; array[3][2] = "17"; array[3][3] = "18";
+        String[][] array = new String[4][4];
+        array[0][0] = "5";
+        array[0][1] = "4";
+        array[0][2] = "3";
+        array[0][3] = "2";
+        array[1][0] = "f";
+        array[1][1] = "8";
+        array[1][2] = "9";
+        array[1][3] = "10";
+        array[2][0] = "11";
+        array[2][1] = "12";
+        array[2][2] = "13";
+        array[2][3] = "14";
+        array[3][0] = "15";
+        array[3][1] = "16";
+        array[3][2] = "17";
+        array[3][3] = "18";
 
         try {
             proccesArray(array);
         } catch (MyArrayDataException e) {
             System.out.println("Ошибка преобразования в строке " + e.getRow() + ", в столбце " + e.getCol());
-
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера");
         }
 
+        String[][] arrayBadRowCount = new String[3][4];
+        try {
+            proccesArray(arrayBadRowCount);
+
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера");
+        }
+        String[][] arrayBadColCount = new String[4][3];
+        try {
+            proccesArray(arrayBadColCount);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера");
+        }
 
     }
 
-    public static void proccesArray(String[][] array) {
-        for(int y = 0; y < 4; y++) {
-            for(int x = 0; x < 4; x++) {
+    public static void proccesArray(String[][] array) throws MyArraySizeException {
+        if (array.length != 4) {
+            MyArraySizeException exp = new MyArraySizeException();
+            throw exp;
+        }
+
+        for(int i = 0; i < array.length;i++) {
+            if(array[i].length != 4) {
+                MyArraySizeException exp = new MyArraySizeException();
+                throw exp;
+            }
+        }
+
+
+        for (int y = 0; y < 4; y++) {
+            for (int x = 0; x < 4; x++) {
                 try {
-                    int i = Integer.parseInt (array[y][x]);
+                    int i = Integer.parseInt(array[y][x]);
                 } catch (NumberFormatException e) {
-                    MyArrayDataException exp = new MyArrayDataException(y,x);
+                    MyArrayDataException exp = new MyArrayDataException(y, x);
                     throw exp;
 
                 }
