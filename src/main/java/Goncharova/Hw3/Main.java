509842b320d313da75fdebcae808fc4d55a0fb3b
Alina Goncharova <al.cherepnina@gmail.com>
2021-03-10 16:32:53 +0300

    Выполнено задание по организации гонок

--- src/main/java/Goncharova/Hw3/Main.java
+++ src/main/java/Goncharova/Hw3/Main.java
@@ -1,126 +1,32 @@
 package Goncharova.Hw3;
 
-import java.util.*;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
+import java.util.concurrent.*;
 
 public class Main {
-
-    static class MyCallableClass implements Callable{
-        float[] arr;
-
-        MyCallableClass(float[] arr) {
-            this.arr = arr;
-        }
-
-        @Override
-        public float [] call() {
-
-            long a = System.currentTimeMillis();
-
-            int i = 0;
-
-            for (i = 0; i < arr.length; i++) {
-                arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-
-            System.out.println(System.currentTimeMillis() - a);
-
-            return arr;
-        }
-    }
-
-    static private ArrayList<float []> splitArray(float [] inputArray, int numberOfParts){
-        ArrayList<float []> L = new ArrayList<>();
-        int partSize = inputArray.length / numberOfParts;
-        int part = 0;
-
-        for(part = 0; part < (numberOfParts-1); part++) {
-
-            float [] temp = new float[partSize];
-            for(int c = 0; c < partSize; c++) {
-                temp[c] = inputArray[c + partSize*part];
-            }
-
-            L.add(temp);
-
-        }
-
-        float [] temp = new  float [inputArray.length - (part * partSize)];
-
-        for(int i = part*partSize, c = 0; i < inputArray.length; i++, c++) {
-            temp[c] = inputArray[i];
-        }
-
-        L.add(temp);
-
-        return L;
-    }
-
-    static private void createArray1(float [] arr) {
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = 1.0f;
-        }
-
-        long a = System.currentTimeMillis();
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
-
-        System.out.println(System.currentTimeMillis() - a);
-
-    }
-
-    static private void createArray2(float [] arr, int numberOfParts) {
-
-        ExecutorService executorService = Executors.newFixedThreadPool(numberOfParts);
-
-        ArrayList<float []> L = splitArray(arr, numberOfParts);
-
-        Future<float []> [] res = new Future[numberOfParts];
-
-        for(int i = 0; i < L.size(); i++){
-            MyCallableClass thread = new MyCallableClass(L.get(i));
-            res[i] = executorService.submit(thread);
-        }
-
-        float [] resArr = new float[arr.length];
-        int pos = 0;
-
-        for(int i = 0; i < numberOfParts;i++) {
-
-            float[] temp;
-
-            try {
-                temp = res[0].get();
-            } catch (Exception e) {
-                System.out.println(e);
-                break;
-            }
-
-            System.arraycopy(temp, 0, resArr, pos, temp.length);
-            pos += temp.length;
-        }
-    }
-
+    public static final int CARS_COUNT = 4;
     public static void main(String[] args) {
 
-        final int size = 10000000;
-        final int half = size / 2;
-        float[] arr = new float[size];
+        Race race = new Race(new Tunnel(CARS_COUNT));
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = 1.0f;
+        Car [] cars = new Car[CARS_COUNT];
+
+        ExecutorService taskExecutor = Executors.newFixedThreadPool(4);
+        for(int i = 0; i < CARS_COUNT;i++) {
+
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            taskExecutor.execute(cars[i]);
         }
 
-        createArray1(arr);
-        createArray2(arr, 2);
+        taskExecutor.shutdown();
+
+        try {
+            taskExecutor.awaitTermination(90, TimeUnit.SECONDS);
+            System.out.println("Победитель гонки " + race.getWinner());
+        } catch (InterruptedException e) {
+
+        }
+
     }
-
-
 }
+
