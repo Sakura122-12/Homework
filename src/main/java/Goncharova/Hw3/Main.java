6833cdde45345eb1ea37e9d4ae7ae5c27a9f22c2
Alina Goncharova <al.cherepnina@gmail.com>
2021-03-13 07:54:15 +0300

    Удалены вызовы из main

--- src/main/java/Goncharova/Hw3/Main.java
+++ src/main/java/Goncharova/Hw3/Main.java
@@ -1,31 +1,13 @@
 package Goncharova.Hw3;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.util.concurrent.*;
 
 public class Main {
-    public static final int CARS_COUNT = 4;
     public static void main(String[] args) {
-
-        Race race = new Race(new Tunnel(CARS_COUNT));
-
-        Car [] cars = new Car[CARS_COUNT];
-
-        ExecutorService taskExecutor = Executors.newFixedThreadPool(4);
-        for(int i = 0; i < CARS_COUNT;i++) {
-
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
-            taskExecutor.execute(cars[i]);
-        }
-
-        taskExecutor.shutdown();
-
-        try {
-            taskExecutor.awaitTermination(90, TimeUnit.SECONDS);
-            System.out.println("Победитель гонки " + race.getWinner());
-        } catch (InterruptedException e) {
-
-        }
 
     }
 }
